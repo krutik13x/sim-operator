3eb2fc2f07d15bf8b0ff732c62cfbacbe0638f74
Aakash Shah <78583334+thisaakash@users.noreply.github.com>
2022-05-16 16:54:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $("#form-sub").submit(function(event) { 
+        performSearch(event); });
+  });
+
+  function performSearch(event) {
+    var request;
+    event.preventDefault();
+    $("#city-name").text("Searching ...");
+  
+    // Send the request  
+   
+    request = $.ajax({
+        url: 'http://apilayer.net/api/validate',
+        type: "GET",
+        data: {  
+          access_key:'1fd83af9dea643ebd5eeb1f4ef098b90',
+          number:$("#city").val(),
+          country_code:'IN',
+          format:'1',
+       }
+    });
+  
+    // Callback handler for success
+    request.done(function (response){
+        formatSearchResults(response);
+    });
+    
+    // Callback handler for failure
+    request.fail(function (){
+        $("#city-name").text("Please try again, incorrect input!");
+        $("#city-temp").text("");
+        $("img").attr('src', "");
+        $("#city-weather").text("");
+    });
+  
+  }
+
+  function formatSearchResults(jsonObject) {  
+    var city_name = jsonObject.carrier; 
+    $("#city-name").text(city_name); 
+  }
+
+
+
+
